fe1fdc989f93c12badd18109b1219a89b21d8a72
Jonathan Pumares <jepc1491@gmail.com>
2021-08-22 00:54:54 -0500

    Práctica de un formulario del curso de VueJS utilizando v-model.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form @submit.prevent="procesarFormulario">
+    <input 
+      type="text" 
+      class="form-control my-2" 
+      placeholder="Ingrese nombre"
+      v-model.trim="tarea.nombre">
+
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox"
+        id="check-1"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="javascript">
+      <label for="check-1" class="form-check-label">JavaScript</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox"
+        id="check-2"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="node js">
+      <label for="check-2" class="form-check-label">Node.js</label>
+    </div>
+
+    <div class="mt-2">
+      <div class="form-check form-check-inline">
+        <input 
+          type="radio"
+          id="radio-1"
+          class="form-check-input"
+          value="urgente"
+          v-model="tarea.estado">
+        <label for="radio-1" class="form-check-label">Urgente</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input 
+          type="radio"
+          id="radio-2"
+          class="form-check-input"
+          value="relax"
+          v-model="tarea.estado">
+        <label for="radio-2" class="form-check-label">Relax</label>
+      </div>
+    </div>
+
+    <div class="mt-2">
+      <input 
+        type="number"
+        class="form-control"
+        v-model.number="tarea.numero">
+    </div>
+
+    <button 
+      class="btn btn-dark btn-block mt-2"
+      type="submit"
+      :disabled="bloquear">Procesar</button>
+  </form>
+  <p>{{ tarea }}</p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      tarea: {
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  methods: {
+    procesarFormulario() {
+      console.log(this.tarea)
+      if (this.tarea.nombre.trim() === '') {
+        console.log('Campo vacío')
+        return
+      }
+      console.log('No esta vacío')
+
+      // Enviar los datos
+
+      this.tarea = {
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  computed: {
+    bloquear() {
+      return this.tarea.nombre.trim() === '' ? true : false
+    }
   }
 }
 </script>
